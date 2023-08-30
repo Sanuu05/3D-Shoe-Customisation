7edeec1917ddccf499c1ffcfd121b1a43138bede
Shantanu Saha <sansaha0505@gmail.com>
2023-08-30 13:05:01 +0530

    update

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -9,7 +9,7 @@ const index = ({rotate}) => {
      <ambientLight intensity={0.7} />
      <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
      <Model />
-     <Environment preset="city" />
+     {/* <Environment preset="city" /> */}
      <ContactShadows position={[0, -0.8, 0]} opacity={0.8} scale={10} blur={1} far={0.8} />
      <OrbitControls  />
    </Canvas>
